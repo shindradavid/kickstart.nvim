752da929cda475e773c1b37be9f5fcea06e15b67
Shindra David <shindradavid.dev@gmail.com>
2023-03-14 17:30:39 +0300

    Add autopairs and highlight colors plugin

---
+++ lua/custom/plugins/highlight-colors.lua
@@ -0,0 +1,12 @@
+vim.cmd([[ set termguicolors ]])
+vim.cmd([[ set t_Co=256 ]])
+
+return {
+  "brenoprata10/nvim-highlight-colors",
+  config = function ()
+    require('nvim-highlight-colors').setup {
+      render = 'first_column'
+    }
+  end,
+}
+
