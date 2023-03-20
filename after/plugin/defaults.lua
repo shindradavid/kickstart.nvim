602697b58ee74e905e874bef8bc125a6e00904c9
Shindra David <shindradavid.dev@gmail.com>
2023-03-20 16:02:19 +0300

    Set wrap to false

--- after/plugin/defaults.lua
+++ after/plugin/defaults.lua
@@ -2,5 +2,7 @@ local opt = vim.opt
 local keymap = vim.keymap
 
 opt.cursorline = true
+opt.wrap = false
 
-keymap.set("n", "<leader>tw", "<cmd>Neotree position=float<cr>")
+keymap.set("n", "<leader>tw", "<cmd>Neotree source=filesystem position=float<cr>")
+keymap.set("n", "<leader>gs", "<cmd>Neotree source=git_status position=float<cr>")
