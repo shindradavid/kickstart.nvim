dd6f23cafa42b0fbcc5c0e2c68417f7ab2cff6c8
Shindra David <shindradavid.dev@gmail.com>
2023-03-20 22:18:43 +0300

    Change Neotree open keymap to 'fs'

--- after/plugin/defaults.lua
+++ after/plugin/defaults.lua
@@ -6,5 +6,6 @@ opt.wrap = false
 opt.relativenumber = true
 opt.background = "dark"
 
-keymap.set("n", "<leader>tw", "<cmd>Neotree source=filesystem position=float<cr>")
-keymap.set("n", "<leader>gs", "<cmd>Neotree source=git_status position=float<cr>")
+-- Neotree key bindings
+keymap.set("n", "<leader>fs", "<cmd>Neotree source=filesystem position=float<cr>", { desc = "Open filesystem" })
+keymap.set("n", "<leader>gs", "<cmd>Neotree source=git_status position=float<cr>", { desc = "View git status" })
