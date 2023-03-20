f113ce47bd61b2f5b60dcf00c0ec7e7cfb7cb87e
Shindra David <shindradavid.dev@gmail.com>
2023-03-20 19:09:58 +0300

    Add background=dark vim option

--- after/plugin/defaults.lua
+++ after/plugin/defaults.lua
@@ -4,6 +4,7 @@ local keymap = vim.keymap
 opt.cursorline = true
 opt.wrap = false
 opt.relativenumber = true
+opt.background = "dark"
 
 keymap.set("n", "<leader>tw", "<cmd>Neotree source=filesystem position=float<cr>")
 keymap.set("n", "<leader>gs", "<cmd>Neotree source=git_status position=float<cr>")
