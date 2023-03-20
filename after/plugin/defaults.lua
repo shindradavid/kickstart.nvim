73b601ac9d34c36ef93627f8db378169dddd5845
Shindra David <shindradavid.dev@gmail.com>
2023-03-20 16:04:46 +0300

    Add relativenumber

--- after/plugin/defaults.lua
+++ after/plugin/defaults.lua
@@ -3,6 +3,7 @@ local keymap = vim.keymap
 
 opt.cursorline = true
 opt.wrap = false
+opt.relativenumber = true
 
 keymap.set("n", "<leader>tw", "<cmd>Neotree source=filesystem position=float<cr>")
 keymap.set("n", "<leader>gs", "<cmd>Neotree source=git_status position=float<cr>")
