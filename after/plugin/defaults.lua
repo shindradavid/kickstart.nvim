96633ee087c4e62b2a08d9c114afb6d697a494ac
Shindra David <shindradavid.dev@gmail.com>
2023-03-26 09:12:25 +0300

    Add convenience keymaps

--- after/plugin/defaults.lua
+++ after/plugin/defaults.lua
@@ -7,7 +7,19 @@ opt.background = "dark"
 opt.backup = false
 opt.swapfile = false
 opt.writebackup = false
+opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+opt.undofile = true
+
 opt.scrolloff = 6
 opt.sidescrolloff = 8
 opt.title = true
 opt.titlestring = "%<%F%=%l/%L"
+
+-- key maps
+
+local keymap = vim.keymap
+
+keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+keymap.set("x", "<leader>p", "\"_dP")
